27b768d6386530b4176c1bc5dfc4d8c3d5edf416
Chris Hafey <chafey@gmail.com>
2021-01-31 22:29:10 +0000

    Made the attribute grouping data driven

--- src/dataSetToIon.js
+++ src/dataSetToIon.js
@@ -6,18 +6,12 @@ const dataSetToIon = (dataSet) => {
     const sortedKeys = Object.keys(dataSet.elements).sort()
     const ionDataSet = {
         vrs: {},
-        uidAttrs: {},
-        patientAttrs: {},
-        studyAttrs: {},
-        seriesAttrs: {},
-        instanceAttrs: {},
-        imageAttrs: {},
-        equipmentAttrs: {},
-        standardAttrs: {},
-        privateAttrs: {},
-
+        groups: {
+            standardAttrs: {},
+            privateAttrs: {}
+        }
     }
-    // TODO: consider smart grouping of attributes (e.g. patient, uids, study)
+
     sortedKeys.map((key) => {
         const attr = dataSet.elements[key]
         //console.log('attr=',attr)
@@ -31,24 +25,24 @@ const dataSetToIon = (dataSet) => {
             // TODO: add VR's for those that match multiple or are not in data dictionary
         }
 
-        if(attrGroups.uidAttrs.has(tag)) {
-            ionDataSet.uidAttrs[attrGroups.uidAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.patientAttrs.has(tag)) {
-            ionDataSet.patientAttrs[attrGroups.patientAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.studyAttrs.has(tag)) {
-            ionDataSet.studyAttrs[attrGroups.studyAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.seriesAttrs.has(tag)) {
-            ionDataSet.seriesAttrs[attrGroups.seriesAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.instanceAttrs.has(tag)) {
-            ionDataSet.instanceAttrs[attrGroups.instanceAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.imageAttrs.has(tag)) {
-            ionDataSet.imageAttrs[attrGroups.imageAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(attrGroups.equipmentAttrs.has(tag)) {
-            ionDataSet.equipmentAttrs[attrGroups.equipmentAttrs.get(tag)] = attrToIon(dataSet, attr)
-        } else if(dicomParser.isPrivateTag(attr.tag)) {
-            ionDataSet.privateAttrs[tag] = attrToIon(dataSet, attr)
-        } else {
-            ionDataSet.standardAttrs[tag] = attrToIon(dataSet, attr)
+        let found = false
+        Object.keys(attrGroups).map((key) => {
+            const group = attrGroups[key]
+            if(group.has(tag)) {
+                if(ionDataSet.groups[key] === undefined) {
+                    ionDataSet.groups[key] = {}
+                }
+                ionDataSet.groups[key][group.get(tag)] = attrToIon(dataSet, attr)
+                found = true
+            }
+        })
+
+        if(found === false) {
+            if(dicomParser.isPrivateTag(attr.tag)) {
+                ionDataSet.groups.privateAttrs[tag] = attrToIon(dataSet, attr)
+            } else {
+                ionDataSet.groups.standardAttrs[tag] = attrToIon(dataSet, attr)
+            }
         }
     })
     return ionDataSet
